4b448a704746306f10c189df72aacff40187a2e4
JoannaRegina <joannaregina1@gmail.com>
2020-03-25 02:44:35 -0300

    Layout da tela inicial

---
+++ frontend-mobile/miaudote/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/components/HomeScreen';
+
+export default function App() {
+  return (
+    <HomeScreen />
+  );
+}
+
---
+++ frontend-mobile/miaudote/src/components/HomeScreen.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+
+
+function HomeScreen() {
+	return (
+		<View style = { styles.container }>
+
+			<View style = { styles.logoContainer }>
+				<Image style= {styles.imagem} source={require('../images/logo.png')} />
+			</View>
+
+			<View style={styles.options}>
+				<TouchableOpacity style={styles.loginButton}>
+					<Text style={styles.loginTextButton}>ENTRAR</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={styles.signUpButton}>
+					<Text style={styles.signUpTextButton}>CRIAR UMA CONTA</Text>
+				</TouchableOpacity>
+
+				<Text style = {styles.txtSkip}>
+					Pular
+				</Text>
+			</View>
+
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	logoContainer: {
+		alignItems: 'center',
+		height: '60%',
+		width: '100%',
+	},
+
+	options: {
+		// flexGrow: 1,
+		height: '40%',
+		width: '100%',
+	},
+
+	imagem: {
+		width: 300,
+		height: 200,
+		margin: 18,
+		top:80
+	},
+
+	loginButton:{
+		alignItems: 'center',
+		height:50,
+		width:300,
+		borderColor:'#5A6978',
+		backgroundColor:'rgba(0, 0, 0, 0.0)',
+		borderWidth: 1.5,
+		padding: 14,
+		marginTop:20,
+		borderRadius:21,
+		alignSelf:"center"
+	},
+
+	signUpButton:{
+		alignItems: 'center',
+		height:50,
+		width:300,
+		borderColor:'#fc6b6e',
+		backgroundColor:'#fc6b6e',
+		borderWidth: 1.5,
+		padding: 14,
+		marginTop:35,
+		borderRadius:21,
+		alignSelf:"center"
+	},
+
+	loginTextButton: {
+		color:'#5A6978',
+		fontSize:15,
+		fontWeight: "bold"
+	},
+
+	signUpTextButton: {
+		color:'#FFF',
+		fontSize:15,
+		fontWeight: "bold"
+	},
+
+	txtSkip: {
+		color: '#1bc7cb',
+		fontSize: 23,
+		alignSelf:"flex-end",
+		position: 'absolute',
+		bottom:25,
+		right:25
+		//   marginBottom: 12
+	}
+
+
+});
+
+export default HomeScreen;
