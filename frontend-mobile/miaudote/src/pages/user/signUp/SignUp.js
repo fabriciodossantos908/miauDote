69c0dcb489095bea8f6145bfdbcd8816a08ee11d
JoannaRegina <joannaregina1@gmail.com>
2020-03-26 19:36:14 -0300

    iniciando tela de cadastro de usuario

--- frontend-mobile/miaudote/src/pages/user/signUp/SignUp.js
+++ frontend-mobile/miaudote/src/pages/user/signUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TextInput, TouchableOpacity, Text, Image } from 'react-native';
 
-import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+import { ProgressSteps, ProgressStep, circleStyle } from 'react-native-progress-steps';
 export default class SignUp extends Component {
 
 	constructor() {
@@ -14,10 +14,6 @@ export default class SignUp extends Component {
 	
 		
 	render() {
-		
-		const activeStepIconBorderColor = {
-			color: '#c789'
-		}
 
 		const buttonTextStyle = {
 			backgroundColor: '#4DB026',
@@ -33,20 +29,37 @@ export default class SignUp extends Component {
 			height: '45px',
 			width: '100px',
 			right: '36px',
-			color:'#fff'
 		}
 
-		
+		const btnTextStyle = {
+			color: '#fff',
+		};
+
+		const progressStepsStyle = {
+			activeStepIconBorderColor: '#686868',
+			activeLabelColor: '#686868',
+			activeStepNumColor: 'white',
+			activeStepIconColor: '#686868',
+			completedStepIconColor: '#686868',
+			completedProgressBarColor: '#686868',
+			completedCheckColor: '#4bb543', 
+		  };
+
+		  const circleStyle = {
+			width: 20,
+			height: 20,
+		  }
+
 			
 			return (
 				<View style={{flex: 1}}>
-					<ProgressSteps>
-						<ProgressStep nextBtnStyle={buttonTextStyle} label="First Step" activeStepIconBorderColor={activeStepIconBorderColor}>
+					<ProgressSteps  circleStyle={circleStyle} {...progressStepsStyle} >
+						<ProgressStep  nextBtnTextStyle={btnTextStyle} nextBtnStyle={buttonTextStyle} label="First Step">
 							<View style={{ alignItems: 'center' }}>
 								<Text>This is the content within step 1!</Text>
 							</View>
 						</ProgressStep>
-						<ProgressStep label="Second Step"  previousBtnStyle={previousButtonStyle} nextBtnStyle={buttonTextStyle} >
+						<ProgressStep previousBtnTextStyle={btnTextStyle} label="Second Step" nextBtnTextStyle={btnTextStyle} previousBtnStyle={previousButtonStyle} nextBtnStyle={buttonTextStyle} >
 							<View style={{ alignItems: 'center' }}>
 								<Text>This is the content within step 2!</Text>
 							</View>
@@ -58,12 +71,3 @@ export default class SignUp extends Component {
 	}
 
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#FFF',
-		justifyContent: "center",
-	},
-
-})
