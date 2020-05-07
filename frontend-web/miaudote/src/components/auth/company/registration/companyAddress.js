8bb4e1669697dcbbe08326ce5c1f3d625f459c7a
Fabrício dos Santos <45518978+fabriciodossantos908@users.noreply.github.com>
2020-05-06 23:21:58 -0300

    Divisão do registro de empresa

---
+++ frontend-web/miaudote/src/components/auth/company/registration/companyAddress.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+export default class Registration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cep: "",
+            cidade: "",
+            bairro: "",
+            logradouro: "",
+            numero: "",
+            complemento: "",
+            uf: "",
+            CompanyAddressErrors: ""
+            }
+
+        this.handleChange = this.handleChange.bind(this)
+
+        }
+
+        handleChange(event) {
+            this.setState({
+                [event.target.name]: event.target.value
+            });
+        }
+
+        handleSubmit(event) {
+            console.log("form enviado!!");
+        }
+        render() {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            name="nome"
+                            placeholder="nome"
+                            value={this.state.nome}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="celular"
+                            placeholder="celular"
+                            value={this.state.celular}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="cpf"
+                            placeholder="cof"
+                            value={this.state.cpf}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="cep"
+                            placeholder="cep"
+                            value={this.state.cep}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="bairro"
+                            placeholder="bairro"
+                            value={this.state.bairro}
+                            onChange={this.handleChange}
+                        />
+
+                        <button type="submit" > Criar</button>
+                    </form>
+                </div>
+            )
+        }
+    }
