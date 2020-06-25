98bca2e19b755173a14867395e3616ac24dbc092
João vitor <49280884+Joaovitordeandrade@users.noreply.github.com>
2020-06-25 08:05:36 -0300

    NavBar

--- frontend-web/miaudote/src/components/baseComponent/Navbar.js
+++ frontend-web/miaudote/src/components/baseComponent/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import { makeStyles, AppBar, Toolbar, Typography, Button, List,
-ListItem, ListItemText } from '@material-ui/core'
+ListItem, ListItemText, Collapse } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  paddingLeft: theme.spacing(4),
 }));
 
 export default function ButtonAppBar() {
@@ -30,19 +31,24 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            LOGO
+            <a href="#">LOGO</a>
           </Typography>
+            <Button color="inherit" primary="Login">Login</Button>  
           <List>
-          <ListItem button onClick={handleClick}>
-            <ListItemText color="inherit" primary="Login" />
-              {open ? <ExpandLess /> : <ExpandMore />}  
-            </ListItem>
-          </List>
-          <List>
-          <ListItem button onClick={handleClick}>
-            <ListItemText color="inherit" primary="Cadastrar" />
-              {open ? <ExpandLess /> : <ExpandMore />}  
-            </ListItem>
+              <ListItem button onClick={handleClick}>
+                <ListItemText color="inherit" primary="Cadastrar" />
+                  {open ? <ExpandLess /> : <ExpandMore />}  
+                </ListItem>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <Button color="inherit">Login</Button>
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <Button color="inherit">Empresa</Button>
+                  </ListItem>
+                </List>
+              </Collapse>
           </List>
         </Toolbar>
       </AppBar>
