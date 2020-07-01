81d75860fc15dd791aeee7cc9dea7e07ee4b402d
João vitor <49280884+Joaovitordeandrade@users.noreply.github.com>
2020-07-01 16:31:24 -0300

    home div

--- frontend-web/miaudote/src/components/Layout/styles.js
+++ frontend-web/miaudote/src/components/Layout/styles.js
@@ -37,11 +37,6 @@ const useStyle = makeStyles(() => ({
     width: 'auto',
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 600,
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    },
     backgroundColor: theme.palette.background.default,
     color: theme.palette.primary.light,
   },
@@ -53,6 +48,7 @@ const useStyle = makeStyles(() => ({
     [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
       marginTop: theme.spacing(6),
       marginBottom: theme.spacing(6),
+      margin: 0
     }
   },
   inputPaper: {
--- frontend-web/miaudote/src/pages/home.js
+++ frontend-web/miaudote/src/pages/home.js
@@ -3,7 +3,7 @@ import { Grid, Paper, makeStyles } from '@material-ui/core';
 
 const useStyle = makeStyles(() => ({
     paper: {
-        width : 600,
+        width : 500,
         height : 600,
     }
 }))
@@ -13,14 +13,17 @@ export default function Home() {
 
     return (
         <React.Fragment>
-                <Grid item container color="primary" direction="row">
-                    <Grid item xs={5} color="primary">
-                        <Paper className={classes.paper}></Paper>
-                    </Grid>
-                    <Grid item xs={5} color="primary">
-                        <Paper className={classes.paper}></Paper>
-                    </Grid>
+            <Grid item container color="primary" direction="row">
+                <Grid item xs={5} color="primary" style={{display: "flex"}}>
+                    <Paper className={classes.paper}>
 
+                    </Paper>
+                
+                
+                    <Paper className={classes.paper}>
+
+                    </Paper>
+                </Grid>
             </Grid>
         </React.Fragment>
     )
