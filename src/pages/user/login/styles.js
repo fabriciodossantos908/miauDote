20f8adff8c99daa094ee6488d98c0c1f6795e00e
JoannaRegina <joannaregina1@gmail.com>
2020-07-03 08:50:59 -0300

    Layout da tela de envio de foto do perfil

---
+++ src/pages/user/login/styles.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components/native'
+
+export const Tittle = styled.Text `
+    color:#fc6b6e;
+    fontSize:45px;
+    fontWeight: bold;
+`;
+
+export const Container = styled.View `
+
+    flex: 1;
+    backgroundColor: #FFF;
+    justifyContent: center;
+    borderWidth: 1px;
+    borderColor:#000;
+
+`;
+
+export const Div = styled.View `
+    flex: 1;
+    justifyContent: center;
+    padding: 10px;
+    alignItems: center;
+    
+`;
+
+// *********global
+export const CuteLine = styled.View `
+    height: 40px;
+    width: 100%;
+    flexDirection: row;
+    justifyContent: center;
+    marginTop:20px;
+`;
+
+
+export const IconPaw = styled.Image `
+    height:20px;
+    width:20px;
+    marginLeft:10px;
+    marginRight:10px;
+
+`;
+
+
+export const LoginForm = styled.View `
+    flex:1;
+    margin:10px;
+    padding:8px;
+    borderRadius: 10px;
+    alignItems: center;
+    backgroundColor:#f7f7f7;
+    paddingTop:15px;
+`;
+
+export const TxtInputLogin = styled.TextInput `
+    height: 50px;
+    marginTop: 10px;
+    padding: 8px;
+    alignSelf: stretch;
+    backgroundColor: white;
+    borderRadius: 10px;
+    borderColor: #a0a0a0;
+    borderWidth: 1px;
+    marginBottom: 10px;
+`;
+
+// **********global
+export const LargeButton = styled.TouchableOpacity `
+    alignItems: center;
+    height:50px;
+    width:300px;
+    borderColor:#fc6b6e;
+    backgroundColor:#fc6b6e;
+    borderWidth: 1.5px;
+    padding: 14px;
+    marginTop:35px;
+    borderRadius:21px;
+    alignSelf:center;  
+`;
+
+export const TxtButon = styled.Text `
+    fontWeight: bold;
+    color: #fff;
+`;
+
+export const ContainerLine = styled.View `
+
+    width: 100%;
+    justifyContent: center;
+    alignItems: center;
+    flexDirection:row;
+    backgroundColor:#fff;
+
+`;
+
+export const HorizontalLine = styled.View `
+    borderWidth: 0.5;
+    borderColor:#5A6978;
+    margin:10px;
+    top:3;
+    width:100;
+`;
+
+
+// *********global
+export const SecondaryText = styled.Text `
+
+    fontSize: 20px;
+    color: #5A6978;
+    letterSpacing: 0.15;
+`;
+
+
+export const ContainerSingInOptions = styled.View `
+    flex:0.5;	
+    justifyContent: center;
+    alignItems: center;
+    flexDirection:row;
+    padding:0;
+`;
+
+export const IconSingInOptions = styled.Image `
+    marginTop:0;
+    height: 70;
+    width: 70;
+    marginLeft:15;
+    marginRight:15;
+`;
+
+export const LoginImage = styled.ImageBackground`
+    height: 256;
+`;
---
+++ src/pages/user/signUp/FormBasicInfo.js
@@ -0,0 +1,191 @@
+import React, { Component } from 'react';
+import { View, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback, ScrollView, StyleSheet, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import {
+	SecundaryTittle,
+} from '../../../components/styles';
+
+import {
+	BtnText,
+	Inner,
+	Title,
+	ImageIcon,
+	ProgressBar,
+	ActiveIcon,
+	DisableIcon,
+	SubtittleContainer,
+	Header,
+	ButtonNext,
+	ContainerButton
+} from './styles';
+
+import { HelperText, TextInput } from 'react-native-paper';
+
+import { TextInputMask } from 'react-native-masked-text'
+
+
+export default class FormBasicInfo extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			email: '',
+			password: '',
+			confirmPassword: ''
+
+		}
+	}
+
+	signUpPage = () => {
+		this.props.navigation.navigate('FormPersonalData')
+		// Alert.alert('aaaaaa')
+	  }
+
+	validate = () => {
+		const { email, password } = this.state
+		if (!email || !password) {
+			alert('Ops... Todos os campos são obrigatórios')
+			return false
+		}
+		return true
+	}
+
+	validatePassword = () => {
+		const { password, confirmPassword } = this.state
+		if (password != confirmPassword) {
+			alert('As senhas não condizem')
+			return false
+		}
+		return true
+	}
+
+
+	nextPage = (props) => {
+		if (!this.validate()) return
+		// else if (validar email)
+		if (!this.validatePassword()) return
+
+		const data = this.state
+
+		console.log(data)
+
+		this.props.navigation.navigate('FormPersonalData', {
+			screen: 'FormBasicInfo',
+			params: { data },
+		});
+
+	}
+
+	render() {
+
+		return (
+			<KeyboardAvoidingView
+				behavior={Platform.OS == "ios" ? "padding" : "height"}
+				// behavior={"height"}
+				style={{flex: 1}}
+			>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+						<Inner>
+							<Header>
+								<Title>Crie sua conta</Title>
+								<ImageIcon source={require('../../../assets/user-account1.png')}></ImageIcon>
+							</Header>
+							<ProgressBar>
+								<ActiveIcon></ActiveIcon>
+								<DisableIcon></DisableIcon>
+								<DisableIcon></DisableIcon>
+							</ProgressBar>
+
+							<View>
+								<SubtittleContainer style={{ marginBottom: 30 }}>
+									<SecundaryTittle style={{ color: '#5A6978' }}>Informações Básicas:</SecundaryTittle>
+								</SubtittleContainer>
+
+								<TextInput
+									style={{ backgroundColor: '#ffff', height: 40, alignSelf: 'stretch', marginBottom:20}}
+									label='Email'
+									keyboardType='email-address'
+									mode={'outlined'}
+									value={this.state.email || ''}
+									onChangeText={txt => this.setState({email: txt})}
+									theme={{
+										// roundness: 50,
+										colors: {
+										primary:'#1bc7cb',
+										underlineColor:'transparent',
+										}
+								}}/>
+
+
+								<TextInput
+									// maxLength={3}
+									style={{ backgroundColor: '#ffff', height: 40, alignSelf: 'stretch', marginBottom:20}}
+									label='Senha' 
+									returnKeyType='go'
+									mode={'outlined'}
+									secureTextEntry={true}
+									value={this.state.password || ''}
+									onChangeText={txt => this.setState({ password: txt })}
+									theme={{
+										// roundness: 50,
+										colors: {
+										primary:'#1bc7cb',
+										underlineColor:'transparent',
+										}
+								}}/>
+
+								<TextInput
+									// maxLength={3}
+									style={{ backgroundColor: '#ffff', height: 40, alignSelf: 'stretch', marginBottom:20}}
+									label='Confirme sua senha' 
+									returnKeyType='go'
+									mode={'outlined'}
+									secureTextEntry={true}
+									value={this.state.confirmPassword || ''}
+									onChangeText={txt => this.setState({ confirmPassword: txt })}
+									theme={{
+										// roundness: 50,
+										colors: {
+										primary:'#1bc7cb',
+										underlineColor:'transparent',
+										}
+								}}/>
+								
+
+							</View>
+
+							{/* <View style={styles.btnContainer}> */}
+								<ContainerButton>
+									<TouchableOpacity style={styles.btn} onPress={this.nextPage}>
+										<BtnText>Próximo</BtnText>
+									</TouchableOpacity>
+								</ContainerButton>
+							{/* </View> */}
+						</Inner>
+					</ScrollView>
+				</TouchableWithoutFeedback>
+			</KeyboardAvoidingView>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	btn: { 
+	  	height: 45,
+		width: 130,
+	  	justifyContent:"center",
+	  	alignItems:"center",
+	  	backgroundColor:'#1bc7cb',
+		borderRadius:5
+	},
+	btnContainer: {
+		// backgroundColor: "white",
+		marginTop: 60,
+		backgroundColor:'#c78',
+		marginLeft:"auto",
+		marginRight:0,
+		// flex:1
+	  },
+});
